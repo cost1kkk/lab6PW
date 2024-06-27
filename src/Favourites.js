4d24a9d126d2eb98e48c24afd41fac5207447b72
Baxanean Constantin <costeamodeon02@mail.ru>
2024-06-27 15:40:15 +0300

    Adding the list for favourites movies

---
+++ src/Favourites.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { useState } from "react";
+import MovieListFavourites from "./MovieListFavourites";
+
+const Favourite = () => {
+
+    const [favourites, setFavourites] = useState([
+            {title: 'Interstellar', description: 'When Earth becomes uninhabitable in the future, a farmer and ex-NASA pilot, Joseph Cooper, is tasked to pilot a spacecraft, along with a team of researchers, to find a new planet for humans.', director: 'Christopher Nolan', image: '/images/interstelar.jpg', id: 1 },
+            {title: 'Django Unchained', description: 'With the help of a German bounty-hunter, a freed slave sets out to rescue his wife from a brutal plantation owner in Mississippi.', director: 'Quentin Tarantino', image: '/images/django.jpg', id: 2 },
+            {title: 'Joker', description: 'During the 1980s, a failed stand-up comedian is driven insane and turns to a life of crime and chaos in Gotham City while becoming an infamous psychopathic crime figure.', director: 'Todd Phillips', image: '/images/joker.jpg', id: 3 },
+            {title: 'Oppenheimer', description: 'The story of American scientist J. Robert Oppenheimer and his role in the development of the atomic bomb.', director: 'Christopher Nolan', image: '/images/openh.jpg', id: 4 }
+        ])
+
+    return ( 
+        <>
+            <MovieListFavourites favourites={favourites} title="Browse your favourites"/>
+        </>
+     );
+}
+ 
+export default Favourite;
