06bd0c7f13fce6527b34d81fe8ab711a0e742404
Baxanean Constantin <costeamodeon02@mail.ru>
2024-06-27 15:43:22 +0300

    Adding the home section

---
+++ src/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Best from "./Best";
+import Trending from "./Trending";
+
+const Home = () => {
+    return ( 
+        <>
+            <Best />
+            <Trending />
+        </>
+     );
+}
+ 
+export default Home;
